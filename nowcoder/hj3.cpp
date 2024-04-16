d075d344a598b5660c47c0c63549a36b9543841e
zfar <lmnsds@163.com>
2024-04-16 10:52:00 +0800

    mingming rand nums: work fine, but official console work bad

---
+++ nowcoder/hj3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int total;
+    int num;
+    int nums[501];
+    while (cin >> total) {
+        memset(nums, 0, sizeof(nums));
+        while(total--) {
+            cin >> num;
+            nums[num] =  1;
+        }
+
+        for (int i =  1; i < 501; ++i) {
+            if (nums[i]) {
+                cout << i << endl;
+            }
+        }
+    }
+}
