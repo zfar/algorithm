fad2c4863fa5a17c11c569b4b9ee1a73a83395b1
zfar <lmnsds@163.com>
2024-04-19 21:16:13 +0800

    job/hw/hw01: 25% ac

---
+++ job/hw/hw01.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+using namespace std;
+
+void break_line(string& str, vector<int>& nums) {
+    int pos = 0;
+    int n_pos = str.find(",");
+    const char* data = str.c_str();
+    while(n_pos != string::npos) {
+        str[n_pos] = 0;
+        nums.push_back(atoi(data + pos));
+        pos = n_pos + 1;
+        n_pos = str.find(",");
+    }
+    nums.push_back(atoi(data + pos));
+}
+
+int get_check_num(const vector<int>& ids, const vector<vector<int> >& rels) {
+    set<int> need_check;
+    for (int i : ids) {
+        vector<int> id_vec;
+        set<int> looked;
+
+        id_vec.push_back(i);
+        looked.insert(i);
+        int pos = 0;
+        while(pos < id_vec.size()) {
+            int id = id_vec[pos];
+
+            // 扩展
+            for (int j = 0; j < rels[i].size(); ++j) {
+                if (id == j)
+                    continue;
+                if (rels[id][j] && looked.find(j) == looked.end()) {
+                    id_vec.push_back(j);
+                    looked.insert(j);
+                    if (need_check.find(j) == need_check.end()) {
+                        need_check.insert(j);
+                    }
+                }
+            }
+
+            ++pos;
+        }
+
+    }
+
+    return need_check.size();
+}
+
+int main() {
+    int n;
+    string ids;
+    while(cin >> n) {
+        // input sicks
+        vector<int> sicks;
+        cin >> ids;
+        break_line(ids, sicks);
+
+        vector<vector<int> > rels;
+        while(n--) {
+            cin >> ids;
+            vector<int> lines;
+            break_line(ids, lines);
+            rels.push_back(lines);
+        }
+        
+        cout << get_check_num(sicks, rels) << endl;
+    }
+    return 0;
+}
---
+++ job/hw/hw02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+using namespace std;
+
+int main() {
+    return 0;
+}
