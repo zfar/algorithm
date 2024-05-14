a4c003bc8e40d7a03189b2a4cd86728fd470409e
zfar <lmnsds@163.com>
2024-05-15 01:15:55 +0800

    just save for 离职

--- job/hw/hw03.cpp
+++ job/hw/hw03.cpp
@@ -8,6 +8,68 @@
 #include <cctype>
 using namespace std;
 
+int dfs(const vector<vector<int> > moneys, int time, int n, int city_index, int time_left, int& sum) {
+    if (city_index = moneys.size()) {
+
+    }
+
+    for (int i = 0; i < time; ++i) {
+
+    }
+    return 0;
+}
+
+int calc_money(int time, int n, const vector<int>& init_m, const vector<int>& des) {
+    if (0 == time) {
+        return 0;
+    }
+
+    vector<vector<int> > moneys;
+    for (int i = 0; i < n; ++i) {
+        vector<int> city_moneys = {0};
+        int init = init_m[i];
+        int d = des[i];
+        int money = init;
+        for (int j = 1; j < time + 1; ++j) {
+            if (money != 0) {
+                city_moneys.push_back(money);
+                money -= d;
+            }
+            else {
+                city_moneys.push_back(0);
+            }
+        }
+    }
+
+    int sum = 0;
+    return dfs(moneys, time, n, 0, time, sum);
+}
+
+// 10 2   // 时间，A城到B城中间的城市数 
+// 1 1 2  // 到各城市需要的天数; 这些城市之间是 线性 的，不是图
+// 200 5  // 中间城市，第一天赚的钱数，和每天递减的数量
+// 90 10
 int main() {
+    int t, n;
+    int val;
+    while (cin >> t >> n) {
+        int tmp = n + 1;
+        int road_time = 0;
+        vector<int> init_m;
+        vector<int> d;
+        while(tmp--) {
+            cin >> val;
+            road_time += val;
+        }
+        tmp = n;
+        while(tmp--) {
+            cin >> val;
+            init_m.push_back(val);
+            cin >> val;
+            d.push_back(val);
+        }
+
+        cout << calc_money(t - road_time, n, init_m, d) << endl;
+    }
     return 0;
 }
--- nowcoder/hj41.cpp
+++ nowcoder/hj41.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <queue>
 using namespace std;
 
 void get_sums(int index, const vector<int> &weights, int sum, const vector<int> &counts,
@@ -15,6 +16,33 @@ void get_sums(int index, const vector<int> &weights, int sum, const vector<int> 
     for (int n = 0; n < count + 1; ++n) {
         get_sums(index + 1, weights, sum, counts, sums);
         sum += single_weight;
+    }
+}
+
+void get_sums_bfs(int num, const vector<int>& weights, vector<int>& counts) {
+    queue<pair<int,int> > queue; // index, value
+
+    // init
+    int i;
+    int weight = weights[0];
+    for (i = 0; i < counts.at(0); ++i) {
+        queue.push(make_pair(0, weight));
+        weight += weights[0];
+    }
+
+    set<int> sums;
+    pair<int,int> node;
+    while(!queue.empty()) {
+
+        node = queue.front();
+        queue.pop();
+
+        // process
+        if (sums.find(node.second) == sums.end()) {
+            sums.insert(node.second);
+        }
+
+        // add node
     }
 }
 
