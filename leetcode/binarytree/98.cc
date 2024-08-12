579c67b8a03b6f8ef56f32a9a5773ab2c8527ac6
zfar <lmnsds@163.com>
2024-08-12 22:58:58 +0800

    leetcode-98: verify the binary search tree, init

---
+++ leetcode/binarytree/98.cc
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ */
+
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> values;
+    bool isValidBST(TreeNode* root) {
+        if (nullptr == root) {
+            return true;
+        }
+
+        bool ret = isValidBST(root->left);
+        if (! ret) {
+            return ret;
+        }
+        if (! values.empty() && values.back() > root->val) {
+            ret = false;
+        }
+
+        values.emplace_back(root->val);
+
+        ret = isValidBST(root->right);
+        if (! ret) {
+            return ret;
+        }
+        if (! values.empty() && values.back() < root->val ) {
+            return false;
+        }
+
+        return true;
+    }
+};
