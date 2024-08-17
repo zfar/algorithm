6b2f0c2739952c74c95afe37b635e39acc939b1e
zfar <lmnsds@163.com>
2024-08-17 11:20:52 +0800

    leetcode-104: init commit, level order traversal

---
+++ leetcode/binarytree/104.cc
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (nullptr == root) {
+            return 0;
+        }
+
+        queue<TreeNode*> nodes;
+        TreeNode* node;
+        nodes.push(root);
+        int size, level = 0;
+        while(! nodes.empty()) {
+            size = nodes.size();
+            while(size--) {
+                node = nodes.front();
+                nodes.pop();
+                if (nullptr != node->left) {
+                    nodes.push(node->left);
+                }
+                if (nullptr != node->right) {
+                    nodes.push(node->right);
+                }
+                ++level;
+            }
+        }
+
+        return level;
+    }
+};
