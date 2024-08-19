76ed7feb02041eeaa678ff0f83cf78bb47092569
zfar <lmnsds@163.com>
2024-08-19 18:05:07 +0800

    leetcode-110: init commit, recursive traversal

---
+++ leetcode/binarytree/110.cc
@@ -0,0 +1,30 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#include <math.h>
+using namespace std;
+
+class Solution {
+public:
+    int deepth(TreeNode* root) {
+        if (nullptr == root) {
+            return 0;
+        }
+
+        int l = deepth(root->left);
+        int r = deepth(root->right);
+        return max(l, r) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        int l = deepth(root->left);
+        int r = deepth(root->right);
+        return abs(l - r) > 1;
+    }
+};
