b9691112ae21ca910fc92e3ed689946a5619b861
zfar <lmnsds@163.com>
2024-08-25 23:24:31 +0800

    leetcode-222: bfs, but calculation is wrong when no node with one child

---
+++ leetcode/binarytree/222.cc
@@ -0,0 +1,44 @@
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (nullptr == root) {
+            return 0;
+        }
+
+        int ret;
+        vector<TreeNode*> nodes;
+        nodes.push_back(root);
+        int i = 0;
+        TreeNode* node;
+        while(i < nodes.size()) {
+            node = nodes.at(i);
+            if (nullptr == node->right) {
+                if (nullptr == node->left) {
+                    ret = nodes.size() * 2 + 1;
+                }
+                else {
+                    ret = nodes.size() * 2;
+                }
+                break;
+            }
+
+            nodes.push_back(node->left);
+            nodes.push_back(node->right);
+            ++i;
+        }
+
+        return ret;
+    }
+};
