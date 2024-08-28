43eeb10d4bd45fd87c69f0ab1b940c9d8a3ef547
zfar <lmnsds@163.com>
2024-08-28 17:16:49 +0800

    leetcode-226: recursion, any order is ok!

---
+++ leetcode/binarytree/226.cc
@@ -0,0 +1,27 @@
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (nullptr == root) {
+            return nullptr;
+        }
+
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        return root;
+    }
+};
