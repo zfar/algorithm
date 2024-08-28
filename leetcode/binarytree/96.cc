7f728c17d836039db8e73ae06a0963ddd3380875
zfar <lmnsds@163.com>
2024-08-28 21:53:59 +0800

    leetcode-96: should divided by (n!)

---
+++ leetcode/binarytree/96.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if (0 == n) {
+            return 1;
+        }
+
+        int ret = 1;
+        for (int i = n + 2; i < 2 * n; ++i) {
+            ret *= i;
+        }
+        ret *= 2; // 2n / n
+
+        int denominator = 1;
+        for (int i = 2; i < n; ++i) {
+            denominator *= i;
+        }
+        return ret / denominator;
+    }
+};
