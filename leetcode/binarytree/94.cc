e04ac59a949c5d16aca878bfec529d9c4a8049fc
zfar <lmnsds@163.com>
2024-08-29 17:23:38 +0800

    leetcode-94: traverse through iteration

---
+++ leetcode/binarytree/94.cc
@@ -0,0 +1,42 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (nullptr == root) {
+            return vector<int>();
+        }
+
+        stack<TreeNode*> nodes;
+        TreeNode* node;
+        vector<int> ret;
+
+        node = root;
+        do {
+            while(nullptr != node) {
+                nodes.push(node);
+                node = node->left;
+            }
+
+            node = nodes.top();
+            nodes.pop();
+            ret.push_back(node->val);
+            if (nullptr != node->right) {
+                node = node->right;
+            }
+        } while (!nodes.empty());
+
+        return ret;
+    }
+};
