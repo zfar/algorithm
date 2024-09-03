2dcf4449ad1864e72722a1d93bc0a21a64a5a171
zfar <lmnsds@163.com>
2024-09-03 22:58:18 +0800

    leetcode-543: post-order traversal, but count depth is wrong!

---
+++ leetcode/binarytree/543.cc
@@ -0,0 +1,36 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#include<math.h>
+using namespace std;
+
+class Solution {
+    int max_d;
+    int countDepth(TreeNode* root) {
+        if (nullptr == root) {
+            return 0;
+        }
+
+        int ld = countDepth(root->left);
+        int rd = countDepth(root->right);
+        int d = ld + rd - 2;
+        if (d > max_d) {
+            max_d = d;
+        }
+
+        return max(ld, rd) + 1;
+    }
+
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        max_d = 0;
+        countDepth(root);
+        return max_d;
+    }
+};
